5a30a0b61068d7df446bd551f4c9ac50ac99f54d
deano1949 <gly1949@gmail.com>
2017-11-05 10:03:24 +0000

    updated Amyaddpath

--- Amyaddpath.m
+++ Amyaddpath.m
@@ -1,16 +1,10 @@
-function Amyaddpath(location)
+function Amyaddpath
 %ADDPATH: add all essential paths
 
-if strcmp(location,'Home')
-    addpath(genpath('C:\Users\Langyu\Desktop\Dropbox\GU\1.Investment\4. Alphas (new)'));
-    addpath(genpath('C:\Users\gly19\Dropbox\GU\1.Investment\4. Alphas (new)\Common_codes'));
-    %data
-    addpath('C:\Spectrion\Data\AllData\Future_Generic');
-elseif strcmp(location,'Coutts')
-    addpath(genpath('O:\langyu\Reading\AlgorithmTrading_Chan_(2013)\Custom_Functions'));
-    addpath('O:\langyu\Reading\AlgorithmTrading_Chan_(2013)');
-else
-    error('Unrecognised location; Coutts or Home');
-end
+addpath(genpath('C:\Users\gly19\Dropbox\GU\1.Investment\4. Alphas (new)\Common_codes'));
+%data
+addpath(genpath('C:\Spectrion\Data\PriceData'));
+addpath(genpath('C:\Spectrion\Data\NewUniverse'));
+
 end
 
